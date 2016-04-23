1b2a18a47768ce3e66cc04d73e62b92cf114675f
An Hoang <pnhanbh@gmail.com>
2016-04-23 17:55:14 +0800

    Test out uploading files

--- src/components/account_setting.js
+++ src/components/account_setting.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import Dropzone from 'react-dropzone';
-import request from 'superagent'
+import { uploadDocuments } from '../actions/account_action'
+
 
 export default class AccountSettingUpload extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.onDrop = this.onDrop.bind(this);
+	}
+
 	onDrop(files) {
-		 console.log('Received files: ', files);
+		 //console.log('Received files: ', files);
+		 this.props.uploadDocuments(files)
 	}
 
 	render() {
 		return (
 			<div>
-				<Dropzone onDrop={this.onDrop}>
+				<Dropzone onDrop={this.onDrop} multiple={true}>
 					<div>Try dropping some files here, or click to select files to upload.</div>
 				</Dropzone>
 			</div>
 		)
 	}
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({uploadDocuments}, dispatch);
+}
+export default connect(null, mapDispatchToProps)(AccountSettingUpload);
