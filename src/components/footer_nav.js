58a273584267839f4c2297e76f4779745eace8d8
An Hoang <pnhanbh@gmail.com>
2016-04-28 18:07:02 +0800

    Add footer

---
+++ src/components/footer_nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class FooterNav extends Component {
+	render() {
+		return (
+			<footer className="footer">
+            	<div className="container">
+                	<p>Catapult by <a href="http://firstmeta.com" title="Catapult - Equity Crowdfunding on Blockchain">First Meta Pte. Ltd. 
+                    </a>© 2016</p>
+            	</div>
+        	</footer>
+		)
+		
+	}
+}
