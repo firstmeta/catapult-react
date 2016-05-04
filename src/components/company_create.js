9542c2844db5481c2ff4bfefe30723ef3014c7ea
hoangan <pnhanbh@gmail.com>
2016-05-04 17:41:38 +0800

    Initialise CompanyCreate components

---
+++ src/components/company_create.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+class CompanyCreate extends Component {
+
+
+  render() {
+    return (
+      <div className="company-create">
+
+        <div className="container-fluid">
+          <div className="row row-centered">
+            <div className="col-lg-1 col-centered">
+            <h1>Let's set up your company!</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="container-fluid">
+          <div className="row row-centered">
+            <div className="col-lg-1 col-centered">
+              <div className="btn-toolbar" role="toolbar">
+                <div className="btn-group" role="group">
+                  <button type="button" className="btn btn-default"><Link to="/companycreate/basic">Basics</Link></button>
+                  <button type="button" className="btn btn-default"><Link to="/companycreate/overview">Overview</Link></button>
+                  <button type="button" className="btn btn-default"><Link to="/companycreate/summary">Summary</Link></button>
+                </div>
+                <div className="btn-group" role="group">
+                  <button type="button" className="btn btn-default"><Link to="/companycreate/preview">Preview</Link></button>
+                </div>
+
+                <div className="btn-group" role="group">
+                  <button type="button" className="btn btn-default"><Link to="/companycreate/submit">Submit</Link></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+    )
+  }
+}
+
+export default CompanyCreate;
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import {Route, IndexRoute} from 'react-router';
 
 import App from './components/Main';
 import Landing from './components/landing';
-import AccountSetting from './components/account_setting'
+import CompanyCreate from './components/company_create';
+import AccountSetting from './components/account_setting';
 
 export default (
 	<Route path="/" component={App}>
 		<IndexRoute component={Landing} />
 		<Route path="api/accountsetting" component={AccountSetting} />
+		<Route path="/companycreate" component={CompanyCreate} />
 	</Route>
 )
