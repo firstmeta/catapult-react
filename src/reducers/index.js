7afe3c66348793d794f5817b36dc87da6f2349f1
hoangan <pnhanbh@gmail.com>
2016-05-20 17:59:08 +0800

    Use redux-router

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import {routerStateReducer} from 'redux-router';
 import AuthReducer from './auth_reducer';
 import AccountReducer from './account_reducer';
 import CompanyReducer from './company_reducer';
 
 const rootReducer = combineReducers({
+	router: routerStateReducer,
 	AuthState: AuthReducer,
 	AccountState: AccountReducer,
 	CompanyState: CompanyReducer
